81fcb261b17aaddd137a0fd731eb81d3c461d3f0
gevhoo <gvahrushev@mail.ru>
2020-04-30 19:24:59 +0500

    Add entry, urls, templates

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,19 @@ class Topic(models.Model):
     def __str__(self):
         # p.399
         return self.text
+
+
+class Entry(models.Model):
+    # p.404
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        # p.404
+        if len(self.text) > 50:
+            return f'{self.text[:50]}...'
+        return self.text
